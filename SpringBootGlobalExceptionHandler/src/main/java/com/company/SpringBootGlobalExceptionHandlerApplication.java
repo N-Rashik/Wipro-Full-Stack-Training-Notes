bfe148ec1effe0013b80d8d51c494403f851d71e
Rashik Nagarajan <98273879+N-Rashik@users.noreply.github.com>
2025-09-09 13:23:24 +0530

    Add files via upload

---
+++ SpringBootGlobalExceptionHandler/src/main/java/com/company/SpringBootGlobalExceptionHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGlobalExceptionHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGlobalExceptionHandlerApplication.class, args);
+	}
+
+}
