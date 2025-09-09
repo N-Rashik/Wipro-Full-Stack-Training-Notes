e22fe8af08a924d989826ede9ca9f079586fa146
Rashik Nagarajan <98273879+N-Rashik@users.noreply.github.com>
2025-09-09 13:23:59 +0530

    Delete GlobalExceptionHandler directory

--- GlobalExceptionHandler/src/main/java/com/example/geh/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.geh.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-	
-	public ResourceNotFoundException(String message) {
-	super(message);
-	}
-}
